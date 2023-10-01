3a3c49794f608b5cc8143fa192c6d3f75fc8e6d7
viveralia <y.ivan.viveros@gmail.com>
2023-09-30 23:54:23 -0600

    fix: resolve correct translations path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
   },
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/locales",
 };
